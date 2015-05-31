c4558abace212af2defb408ada26632b68c28533
jamadagniVijay <vijay.jamadagni@gmail.com>
2015-05-31 22:31:31 +0530

    added spinner

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,4 +1,4 @@
-RateImpactorApp=angular.module('RateImpactorApp', ['ngRoute','uiSlider','ui-rangeSlider']);
+RateImpactorApp=angular.module('RateImpactorApp', ['ngRoute','uiSlider','ui-rangeSlider','angularSpinner']);
 
 RateImpactorApp.controller('GAICAppController', ['$scope',function($scope ) {
 
@@ -140,6 +140,31 @@ RateImpactorApp.controller('rateImactorController',['$scope','$location','$http'
 	});*/
 	$scope.simiulateDataLoad = function()
 	{
+		var target = document.getElementById('chartContainer');
+		var opts = {
+				  lines: 24 // The number of lines to draw
+				, length: 28 // The length of each line
+				, width: 2 // The line thickness
+				, radius: 42 // The radius of the inner circle
+				, scale: 1 // Scales overall size of the spinner
+				, corners: 1 // Corner roundness (0..1)
+				, color: '#3690C5' // #rgb or #rrggbb or array of colors
+				, opacity: 0.25 // Opacity of the lines
+				, rotate: 0 // The rotation offset
+				, direction: 1 // 1: clockwise, -1: counterclockwise
+				, speed: 1 // Rounds per second
+				, trail: 60 // Afterglow percentage
+				, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+				, zIndex: 2e9 // The z-index (defaults to 2000000000)
+				, className: 'spinner' // The CSS class to assign to the spinner
+				, top: '50%' // Top position relative to parent
+				, left: '50%' // Left position relative to parent
+				, shadow: true // Whether to render a shadow
+				, hwaccel: true // Whether to use hardware acceleration
+				, position: 'absolute' // Element positioning
+				}
+
+		var spinner = new Spinner(opts).spin(target);
 		
 		$http.get('rateService/rateimpactor/getPremium/revised').success(function (data) {
 			if(revised.length==0)
@@ -151,6 +176,7 @@ RateImpactorApp.controller('rateImactorController',['$scope','$location','$http'
 						label:label[i]
 					});
 				}
+				spinner.stop();
 				chart.render();
 			}
 		});
